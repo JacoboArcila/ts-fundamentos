eca49faddaf6bc0794091210d1b03a1fa7f78f06
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-01 12:15:00 -0500

    05 booleans

--- src/04-numbers.ts
+++ src/04-numbers.ts
@@ -18,8 +18,12 @@
   let discount = parseInt('123');
   console.log('discount', discount);
 
-
   let hex = 0xfff;
   console.log('hex', hex);
+
+  let bin = 0b1010;
+  console.log('bin', bin);
+
+
 
 })();
