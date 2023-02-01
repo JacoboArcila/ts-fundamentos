57c17cf6daf19532454be7391f87185b414ba1da
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-02-01 12:06:58 -0500

    04-Numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,25 @@
+(() => {
+
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 28;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+
+  //si quiero poner una variable y no le asigno valor si le tengo que especificar el tipo de dato que va a guardar.
+  let productInStock: number;
+  console.log('productInStock', productInStock);
+  if(productInStock > 10){
+    console.log('is greater');
+  }
+
+  let discount = parseInt('123');
+  console.log('discount', discount);
+
+
+  let hex = 0xfff;
+  console.log('hex', hex);
+
+})();
